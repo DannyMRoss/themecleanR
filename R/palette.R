45d70b529cfd781a2f4bc0f9b9c511030931fd51
dross <dross@epsiloneconomics.com>
2023-07-12 16:31:47 -0500

    colors update

--- R/palette.R
+++ R/palette.R
@@ -1,35 +1,75 @@
-# Colors for clean palette
-clean_colors <- c("#1F4E79", "#D00000", "#C49500", "#517D33", "#9B4F96",
-                  "#00A3AD", "#FF5733", "#FFB6C1",
-                  "#F2D49B", "#C7E5F0","#F8766D", "#C49A6C",
-                  "#53B3CB", "#00BA38", "#FC8D62", "#8DA0CB",
-                  "#66C2A5", "#E78AC3", "#A6D854", "#FF7F00",
-                  "#B2DF8A", "#33A02C", "#FB9A99", "#BEBADA",
-                  "#CCEBC5", "#FFC0CB", "#D9D9D9", "#BC80BD",
-                  "#CEDB9C", "#FFFFB3")
+#' Clean colors
+#'
+#' @name clean_colors
+#' @export
+clean_colors <- list(
+  blue = "#1F4E79",
+  red = "#D00000",
+  gold = "#C49500",
+  green = "#517D33",
+  purple = "#9B4F96",
+  cyan = "#00A3AD",
+  salmon = "#FF5733",
+  pink = "#FFB6C1",
+  tan = "#F2D49B",
+  light_blue = "#C7E5F0",
+  light_red = "#F8766D",
+  light_brown = "#C49A6C",
+  turquoise = "#53B3CB",
+  lime = "#00BA38",
+  light_orange = "#FC8D62",
+  light_purple = "#8DA0CB",
+  mint = "#66C2A5",
+  lavender = "#E78AC3",
+  light_green = "#A6D854",
+  dark_orange = "#FF7F00",
+  pale_green = "#B2DF8A",
+  dark_green = "#33A02C",
+  light_pink = "#FB9A99",
+  pale_lavender = "#BEBADA",
+  light_mint = "#CCEBC5",
+  light_pink2 = "#FFC0CB",
+  gray = "#D9D9D9",
+  dark_purple = "#BC80BD",
+  pale_yellow = "#CEDB9C",
+  light_yellow = "#FFFFB3"
+)
 
 #' Clean color palette
 #'
 #' @param n Number of colors
-#' @param colors Vector of colors
+#' @param colors Named list of colors
 #' @param type Discrete or continuous scale
+#' @param order Colors to move to the front
 #'
 #' @return Clean palette object
 #' @export
-clean_palette = function(n, colors = clean_colors, type = c("discrete", "continuous")) {
-  if (is.null(colors)){
+clean_palette = function(n, colors = clean_colors, type = c("discrete", "continuous"), order = NULL) {
+  if(is.null(colors)){
     colors <- clean_colors
   }
-  if (missing(n)) {
-    n = length(colors)
+
+  if (!is.null(order)) {
+    ordered_colors <- unlist(colors[order])
+    remaining_colors <- unlist(colors[setdiff(names(colors), order)])
+    colors <- as.vector(c(ordered_colors, remaining_colors))
+  } else {
+    colors <- as.vector(unlist(colors))
   }
-  type = match.arg(type)
-  out = switch(type,
-               continuous = grDevices::colorRampPalette(colors)(n),
-               discrete = colors[1:n]
+
+  if (missing(n)) {
+    n <- length(colors)
+  }
+  type <- match.arg(type)
+  out <- switch(
+    type,
+    continuous = grDevices::colorRampPalette(colors)(n),
+    discrete = colors[1:n]
   )
   structure(out, class = "palette")
 }
+
+
 
 #' Clean manual color scale for ggplots
 #'
